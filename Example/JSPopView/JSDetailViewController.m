ef187b77355279bb4cf8f8a1e2b066978c6fb322
luomengge <luomenge@meicai.cn>
2019-07-01 16:58:46 +0800

    新增了两个文件

---
+++ Example/JSPopView/JSDetailViewController.m
@@ -0,0 +1,34 @@
+//
+//  JSDetailViewController.m
+//  JSPopView_Example
+//
+//  Created by 罗孟歌 on 2019/7/1.
+//  Copyright © 2019 lmg4819. All rights reserved.
+//
+
+#import "JSDetailViewController.h"
+
+@interface JSDetailViewController ()
+
+@end
+
+@implementation JSDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
